9c2dd0360109412bc87ff718562c7addc8955e8f
Valentina Ivanova <iv.valya.vi85@gmail.com>
2022-12-08 15:18:51 +0300

    fix: fix path directory and add package ts, eslint

--- src/components/controller/appLoader.js
+++ src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: '2b0c1a3e6eb74a5091eb5695b9ad6404', // получите свой ключ https://newsapi.org/
         });
     }
 }
